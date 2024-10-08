7b9985e68d3a4d4420d5cb03d7a5b20a084a6e8a
IsaakCov <isaakcovarrubias@gmail.com>
2024-10-07 23:59:52 -0300

    inicio panol en react

--- src/productoSeleccionado/productoSeleccionado.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('nav ul li a').forEach(item => {
-    item.addEventListener('click', event => {
-        document.querySelectorAll('nav ul li a').forEach(link => {
-            link.classList.remove('selected');
-        });
-        event.target.classList.add('selected');
-    });
-});
