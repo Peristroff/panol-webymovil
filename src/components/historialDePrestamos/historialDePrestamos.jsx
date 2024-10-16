874c0ad144665abfbf179155d2e0bbe5009db26f
IsaakCov <isaakcovarrubias@gmail.com>
2024-10-16 13:50:06 -0300

    x

--- src/components/historialDePrestamos/historialDePrestamos.jsx
+++ src/components/historialDePrestamos/historialDePrestamos.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { Table, Button, Form, Container, Row, Col, InputGroup } from 'react-bootstrap';
+import { Table, Button, Container, Row, Col } from 'react-bootstrap';
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
 import './historialDePrestamos.css'; // Archivo CSS para personalización
 
 function HistorialDePrestamos() {
@@ -11,19 +13,21 @@ function HistorialDePrestamos() {
         { id: 5, name: 'Cable USB', user: 'Gema', date: '15/08/24', returnDate:'13/10/24', category: 'Categoría 3', userAsk: 5, userReceived:1, stock: 69, price: '30000' },
     ]);
 
-    const handleAccept = (id) => {
-        console.log(`El item ${id}, fue aceptado`);
-        // Lógica para aceptar el ítem
-    };
-
-    const handleReject = (id) => {
-        console.log(`Eliminando ítem con ID: ${id}`);
-        // Lógica para eliminar el ítem
-    };
-
-    const handleCreateNewItem = () => {
-        console.log('Creando un nuevo ítem');
-        // Lógica para crear un nuevo ítem
+    const generatePDF = (item) => {
+        const doc = new jsPDF();
+        doc.text('Disfruta tu producto en el pañol, recuerda devolverlo a tiempo!');
+        doc.text('Recibo de Préstamo', 20, 20);
+        doc.text(`ID: ${item.id}`, 20, 30);
+        doc.text(`Nombre de producto: ${item.name}`, 20, 40);
+        doc.text(`Usuario: ${item.user}`, 20, 50);
+        doc.text(`Fecha de petición: ${item.date}`, 20, 60);
+        doc.text(`Fecha de devolución: ${item.returnDate}`, 20, 70);
+        doc.text(`Categoría: ${item.category}`, 20, 80);
+        doc.text(`Cantidad pedida: ${item.userAsk}`, 20, 90);
+        doc.text(`Stock: ${item.stock}`, 20, 100);
+        doc.text(`Cantidad a entregar: ${item.userReceived}`, 20, 110);
+        doc.text(`Precio por unidad: ${item.price}`, 20, 120);
+        doc.save(`prestamo_${item.id}.pdf`);
     };
 
     return (
@@ -31,7 +35,6 @@ function HistorialDePrestamos() {
             <Row>
                 <Col>
                     <h2 className="text-center">Historial de Préstamos</h2>
-
                     <Table striped bordered hover>
                         <thead>
                         <tr>
@@ -61,9 +64,11 @@ function HistorialDePrestamos() {
                                 <td>{item.stock}</td>
                                 <td>{item.userReceived}</td>
                                 <td>{item.price}</td>
-                                <td><Button variant="outline-primary" onClick={() => handleReject(item.id)} className="mb-2">
+                                <td>
+                                    <Button variant="outline-primary" onClick={() => generatePDF(item)} className="mb-2">
                                         Descargar
-                                    </Button></td>
+                                    </Button>
+                                </td>
                             </tr>
                         ))}
                         </tbody>
